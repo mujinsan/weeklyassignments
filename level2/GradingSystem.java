a63e5a3c063b7e2e8e473fc573b99fc59849df3c
mujinsan <mujinsan@users.noreply.github.com>
2026-03-15 23:13:42 +0530

    Add GradingSystem.java (Problem 2) to level2

---
+++ level2/GradingSystem.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class GradingSystem {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter marks: ");
+        int marks = input.nextInt();
+
+        if (marks >= 90) {
+            System.out.println("Grade: A+");
+        } else if (marks >= 75) {
+            System.out.println("Grade: A");
+        } else if (marks >= 60) {
+            System.out.println("Grade: B");
+        } else if (marks >= 40) {
+            System.out.println("Grade: C");
+        } else {
+            System.out.println("Grade: F (Fail)");
+        }
+    }
+}
